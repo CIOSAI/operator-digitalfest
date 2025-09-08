9a7e0c2ead590453ac87367b88d58d694f0ca1d9
jy <jyinteractive.tw@gmail.com>
2025-09-09 01:06:43 +0800

    made discord invite single dependent

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let wips = document.getElementsByClassName("WIP");
 for (let wip of wips) {
 	wip.title = "🙇 未完工/WIP 🙇";
 }
+
+const DISCORD_INVITE = "https://discord.gg/HSwYqDc8Na"
+const invite_anchor = document.getElementById("discord-invite");
+if (invite_anchor) { 
+	invite_anchor.href = DISCORD_INVITE;
+	invite_anchor.innerText = DISCORD_INVITE;
+}
