c9105fa2aad7e326b20e4288779354113fa0d548
jy <jyinteractive.tw@gmail.com>
2025-09-09 01:14:41 +0800

    refactor initialization

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let wips = document.getElementsByClassName("WIP");
+const DISCORD_INVITE = "https://discord.gg/HSwYqDc8Na"
+
+function initialize() {
+const wips = document.getElementsByClassName("WIP");
 for (let wip of wips) {
 	wip.title = "🙇 未完工/WIP 🙇";
 }
 
-const DISCORD_INVITE = "https://discord.gg/HSwYqDc8Na"
 const invite_anchor = document.getElementById("discord-invite");
 if (invite_anchor) { 
 	invite_anchor.href = DISCORD_INVITE;
 	invite_anchor.innerText = DISCORD_INVITE;
 }
+}
+
+initialize();
