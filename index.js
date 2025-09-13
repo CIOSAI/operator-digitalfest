953ef9d46ccb61ea988bbf108fb3372b19c75221
jy <jyinteractive.tw@gmail.com>
2025-09-13 16:55:09 +0800

    language changer

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ if (invite_anchor) {
 	invite_anchor.href = DISCORD_INVITE;
 	invite_anchor.innerText = DISCORD_INVITE;
 }
+
+const change_language = document.createElement("a");
+change_language.appendChild(document.createTextNode("中/Eng"));
+change_language.id = "change-language";
+const at_page = window.location.href.match(/operator-digitalfest(\/?.*)/)[1];
+const is_en = /\/en/g.test(at_page);
+if (at_page.match("timetable")) {
+	change_language.href = "https://ciosai.github.io/operator-digitalfest/timetable" + (is_en?"":"/en");
+}
+else if (at_page.match("compo-rules")) {
+	change_language.href = "https://ciosai.github.io/operator-digitalfest/compo-rules" + (is_en?"":"/en");
+}
+else if (at_page.match("about")) {
+	change_language.href = "https://ciosai.github.io/operator-digitalfest/about" + (is_en?"":"/en");
+}
+else if (at_page.match("contact")) {
+	change_language.href = "https://ciosai.github.io/operator-digitalfest/contact" + (is_en?"":"/en");
+}
+else {
+	change_language.href = "https://ciosai.github.io/operator-digitalfest" + (is_en?"":"/en");
+}
+document.body.appendChild(change_language);
 }
 
 initialize();
